d43ee1c941a98a784cf011eb5b6defe0521bb654
Kirya <shevtsovdev@gmail.com>
2017-01-20 16:14:09 +0200

    add geocoder

--- js/routing-control.js
+++ js/routing-control.js
@@ -1,6 +1,12 @@
+// Add Geocoder
+L.Control.geocoder().addTo(mymap);
+
+
 L.Routing.control({
 	waypoints: [
     L.latLng(49.98315, 36.23913),
     L.latLng(46.47304, 30.75095)
-  ]
+  ],
+    routeWhileDragging: true,
+    geocoder: L.Control.Geocoder.nominatim()
 }).addTo(mymap);
